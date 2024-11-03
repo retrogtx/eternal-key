3c5666614526b72ba1fa6c0035707e389da3f155
Amrit <iamamrit27@gmail.com>
2024-11-03 09:15:50 +0530

    smart contract is live

--- switch/programs/switch/src/lib.rs
+++ switch/programs/switch/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AwPCqYWqJA2N1oZRNvt4wRUFNnH8kij4kuiKNNomSY7F");
 
 #[program]
 pub mod switch {
